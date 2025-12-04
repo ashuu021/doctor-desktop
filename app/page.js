231eafdaa8d59644058394f2a8df84e3bda15491
ashuu021 <ashuu023@gmail.com>
2025-12-04 18:56:34 +0530

    update Home component with padding and adjust header navigation styling

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
-  return <div> Hello I am Anshu
+  return <div className="py-22"> Hello I am Anshu
     <br/>
     <Button>Click me</Button>
      </div>
--- components/header.jsx
+++ components/header.jsx
@@ -5,13 +5,13 @@ import React from "react";
 const Header = () => {
   return (
     <header className=" fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-10 supports-backdrop-filter:bg-baclground/60" >
-      <nav>
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
             src="/logo.png"
             alt="doctor-desktop-logo"
             width={200}
-            height={60}
+            height={80}
             className="h-10 w-auto object-contain"
           />
         </Link>
